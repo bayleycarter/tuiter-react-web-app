71473990baada500d774d85d0d984fab5a1e783b
Bayley Carter <carter.ba@northeastern.edu>
2022-10-23 19:44:36 -0400

    comment warning fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
-import HomeComponent from "../src/tuiter/home/index.js"
 
 function App() {
   return (
